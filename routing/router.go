ccd15c50d75d0f4c43cd84c89c49c120cfbf1d3a
Lei Yang <lei@yangl1996.com>
2018-11-17 14:51:21 -0500

    ignore channel capacity and pruned edge/vertex when finding shortest path

--- routing/router.go
+++ routing/router.go
@@ -1648,7 +1648,7 @@ func (r *ChannelRouter) SendSpider(payment *LightningPayment, spiderAlgo int) ([
 		}
 
 		// get optimal route
-		route, err = dummyPaySession.RequestRoute(
+		route, err = dummyPaySession.RequestShortestPath(
 			payment, uint32(currentHeight), finalCLTVDelta,
 		)
 		if err != nil {
